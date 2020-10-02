9c2f10a0e2b1d32c303220aba3a9e40071bcbe8d
Gabriel Coutinho <coutinho.araujo.gabriel@gmail.com>
2020-10-02 16:09:23 -0300

    Initial commit

---
+++ src/main/java/com/gabrielcoutinho/numberguesser/NumberguesserApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielcoutinho.numberguesser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberguesserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberguesserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielcoutinho/numberguesser/NumberguesserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielcoutinho.numberguesser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberguesserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
