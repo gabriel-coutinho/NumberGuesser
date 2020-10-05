750691d2589ffd2b8f06ce3a301453f10993a101
Gabriel Coutinho <coutinho.araujo.gabriel@gmail.com>
2020-10-05 09:29:34 -0300

    Fix on h2-console

--- src/main/java/com/gabrielcoutinho/numberguesser/config/SecurityConfig.java
+++ src/main/java/com/gabrielcoutinho/numberguesser/config/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.headers().frameOptions().disable();
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
